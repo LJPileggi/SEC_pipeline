17beab581c65b4e0cfc5de0d7c6ed82887d2e1d0
LJPileggi <l.pileggi1@studenti.unipi.it>
2025-11-17 19:28:29 +0100

    Minor.

--- src/utils.py
+++ src/utils.py
@@ -302,7 +302,7 @@ class HDF5EmbeddingDatasetsManager(Dataset):
         self.hash_keys_buffer.append(hash_keys)
         self.track_names_buffer.append(track_name)
         self.classes_buffer.append(class_ if class_ else [None] * len(embedding))
-        self.subclasses_buffer.append(subclass if subclass else [None] * len(embedding)
+        self.subclasses_buffer.append(subclass if subclass else [None] * len(embedding))
 
     def flush_buffers(self):
         """
