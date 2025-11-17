1e34edf07c3d8a2bdd572ae9b9732f587030e26e
LJPileggi <l.pileggi1@studenti.unipi.it>
2025-11-17 19:47:53 +0100

    Added main script to run tests.

---
+++ run_tests.sh
@@ -0,0 +1,24 @@
+# Definizione delle variabili principali per il lancio
+USER_SCRATCH="/leonardo_scratch/large/userexternal/$USER"
+PROJECT_ROOT_DIR="$USER_SCRATCH/SEC_pipeline" 
+SIF_PATH="$PROJECT_ROOT_DIR/.containers/clap_pipeline.sif"
+
+# La cartella che contiene i pesi e i dati
+DATA_ROOT="$USER_SCRATCH/dataSEC"
+WEIGHTS_DIR="$PROJECT_ROOT_DIR/.clap_weights"
+
+
+# Comando di Esecuzione (singularity exec)
+if ["$1" == "test_latency.sh"]; then
+    singularity exec \
+        --bind $DATA_ROOT:/app/data \
+        --bind $WEIGHTS_DIR:/app/weights \
+        $SIF_PATH \
+        "./tests/$1"
+else
+    singularity exec \
+        --bind $DATA_ROOT:/app/data \
+        --bind $WEIGHTS_DIR:/app/weights \
+        $SIF_PATH \
+        python3 tests/"$1"
+fi
