66c6258744625c0c803824c51db45c59878aceea
LJPileggi <l.pileggi1@studenti.unipi.it>
2025-12-04 12:55:59 +0100

    Minor.

--- src/distributed_clap_embeddings.py
+++ src/distributed_clap_embeddings.py
@@ -107,6 +107,7 @@ def process_class_with_cut_secs(clap_model, audio_embedding, class_to_process, c
         while True:
             round_ += 1
             for metadata in perms_metadata:
+                print(metadata)
                 track_idx = metadata['hdf5_index']
                 track = audio_dataset_manager[track_idx]
                 window_size = int(cut_secs * sr)
