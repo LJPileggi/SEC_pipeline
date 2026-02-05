c83309a6bc751d563eb3882c48c33e31a9b445a8
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-02-05 15:15:46 +0100

    Minor.

--- alia/IDOFASC_HPC.py
+++ alia/IDOFASC_HPC.py
@@ -156,7 +156,8 @@ def main():
     # Plot Polygons (Convex Hull) con correzione per stabilità numerica
     color_list = [cm.get_cmap("tab20" if len(classes) <= 20 else "hsv", len(classes))(i) for i in range(len(classes))]
     for (name, algo), res in results.items():
-        X2 = PCA(n_components=2).fit_transform(res["data"])
+        pca_plot = PCA(n_components=2)
+        X2 = pca_plot.fit_transform(res["data"])
         plt.figure(figsize=(10, 8))
         
         for c in range(len(classes)):
@@ -182,7 +183,9 @@ def main():
             plt.text(np.mean(pts[:, 0]), np.mean(pts[:, 1]), str(c+1), fontsize=12, fontweight='bold')
         
         # Centroidi originali (già scalati PC1-PC2)
-        plt.scatter(res["model"].cluster_centers_[:, 0], res["model"].cluster_centers_[:, 1], c="red", s=80, marker="X")
+        centroids_x1, centroids_x2 = pca_plot.fit_transform(res["model"].cluster_centers_[:, 0]), \
+                                   pca_plot.fit_transform(res["model"].cluster_centers_[:, 1])
+        plt.scatter(centroids_x1, centroids_x2, c="red", s=80, marker="X")
         plt.title(f"{name.upper()} – {algo.capitalize()} ({audio_format})")
         plt.savefig(os.path.join(output_folder, f"plot_{name}_{algo}_{audio_format}.png"), dpi=600)
         plt.close()
