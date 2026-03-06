1ffba34394292695df2c2d668c4e6db054b05ae6
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-03-06 13:17:25 +0100

    Minor.

--- src/losses.py
+++ src/losses.py
@@ -69,7 +69,7 @@ def get_scores(model, test_dataloader, train_dataloader=None):
         for x, y in tqdm(test_dataloader, desc=f'loss on test'):
             p = model(x)
             p_list += p.argmax(axis=-1).tolist()
-            y_list += y.argmax(axis=-1).tolist()
+            y_list += y.tolist() # y.argmax(axis=-1).tolist()
             ts_loss += _criterion(p, y).item()
             ts_accuracy += accuracy(y, p)
         if train_dataloader is not None:
