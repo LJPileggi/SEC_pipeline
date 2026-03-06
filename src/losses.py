77b3095fcd4fa9b4c8ee448d0aea561aece32881
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-03-06 17:25:32 +0100

    Minor.

--- src/losses.py
+++ src/losses.py
@@ -72,7 +72,7 @@ def get_scores(model, test_dataloader, train_dataloader=None):
             p_list += p.argmax(axis=-1).tolist()
             y_list += y.tolist() # y.argmax(axis=-1).tolist()
             print(y)
-            print(p.argmax(axis=-1)
+            print(p.argmax(axis=-1))
             ts_loss += _criterion(p, y).item()
             ts_accuracy += accuracy(y, p)
             print(ts_accuracy)
