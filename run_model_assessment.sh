2c92a656b73ddb5bce8b4d468847e05a038c3326
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-05-18 13:05:02 +0200

    Minor.

--- run_model_assessment.sh
+++ run_model_assessment.sh
@@ -55,9 +55,22 @@ mkdir -p "$L_TMP/embeddings"
 # --- 3. PREPARE TASK LIST (GROUND TRUTH: Ricerca sull'albero) ---
 cd "${EMB_BASE}"
 # Troviamo i file relativi partendo dalla root del dataset
-mapfile -t H5_TRAIN_LIST < <(find "./${AUDIO_FORMAT}/${target_folder}" -path "*/7_secs/combined_train.h5" | sed 's|^\./||')
-mapfile -t H5_VALID_LIST < <(find "./${AUDIO_FORMAT}/${target_folder}" -path "*/7_secs/combined_valid.h5" | sed 's|^\./||')
-mapfile -t H5_ES_LIST < <(find "./${AUDIO_FORMAT}/${target_folder}" -path "*/7_secs/combined_es.h5" | sed 's|^\./||')
+if [ "$N_OCTAVE" -eq 0 ]; then
+    # Macro-gruppo Audio Grezzo: prende solo 0_octave
+    mapfile -t H5_TRAIN_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/0_octave/7_secs/combined_train.h5" | sed 's|^\./||')
+    mapfile -t H5_VALID_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/0_octave/7_secs/combined_valid.h5" | sed 's|^\./||')
+    mapfile -t H5_ES_LIST    < <(find "./${AUDIO_FORMAT}" -type f -path "*/0_octave/7_secs/combined_es.h5" | sed 's|^\./||')
+elif [ "$INJECT_OCTAVE_CMD" = "False" ]; then
+    # Macro-gruppo No Injection: prende tutte le cartelle con no_inject (esclude 0_octave)
+    mapfile -t H5_TRAIN_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_train.h5" -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+    mapfile -t H5_VALID_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_valid.h5" -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+    mapfile -t H5_ES_LIST    < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_es.h5" -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+else
+    # Macro-gruppo Injection: prende tutte le cartelle standard (esclude no_inject e 0_octave)
+    mapfile -t H5_TRAIN_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_train.h5" ! -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+    mapfile -t H5_VALID_LIST < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_valid.h5" ! -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+    mapfile -t H5_ES_LIST    < <(find "./${AUDIO_FORMAT}" -type f -path "*/7_secs/combined_es.h5" ! -path "*no_inject*" ! -path "*0_octave*" | sed 's|^\./||')
+fi
 cd "$PROJECT_DIR"
 
 # --- 4. CREATE TEMPORARY AGGREGATOR SCRIPT IN .tmp ---
