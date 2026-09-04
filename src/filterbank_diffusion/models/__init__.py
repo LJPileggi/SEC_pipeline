a137849198ab96c58ea84a8045431dd4bd65ae03
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-09-04 19:24:36 +0200

    Minor.

--- src/filterbank_diffusion/models/__init__.py
+++ src/filterbank_diffusion/models/__init__.py
@@ -1,4 +1,4 @@
 from .unet import SpectrogramUNet
-from .diffusion import GaussianDiffusion
+from .diffusion import GaussianDiffusionResidual
 
-__all__ = ["SpectrogramUNet", "GaussianDiffusion"]
+__all__ = ["SpectrogramUNet", "GaussianDiffusionResidual"]
