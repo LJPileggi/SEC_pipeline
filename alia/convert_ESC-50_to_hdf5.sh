74b3677fdc27e8e052be9aab72d312da73ef7ffe
LJPileggi <l.pileggi1@studenti.unipi.it>
2026-09-23 12:37:56 +0200

    Minor.

--- alia/convert_ESC-50_to_hdf5.sh
+++ alia/convert_ESC-50_to_hdf5.sh
@@ -3,7 +3,7 @@ export SIF_FILE="$PROJECT_DIR/.containers/clap_pipeline.sif"
 
 # Trova il percorso assoluto reale del file di conversione
 SCRIPT_PATH="$(find "$PROJECT_DIR" -name "convert_ESC50_to_hdf5.py" 2>/dev/null | head -n 1)"
-[ -z "$SCRIPT_PATH" ] && SCRIPT_PATH="$(pwd)/convert_ESC50_to_hdf5.py"
+[ -z "$SCRIPT_PATH" ] && SCRIPT_PATH="$(pwd)/alia/convert_ESC50_to_hdf5.py"
 
 singularity exec --no-home \
     --bind "/leonardo_scratch:/leonardo_scratch" \
